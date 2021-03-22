e68b082659bce906211fc0eeeae3b553708f09b5
Damian Orzechowski <damianmisiek110@gmail.com>
2021-03-22 23:14:27 +0100

    dorobić src

---
+++ my-app/src/components/TopBar/ExpandedMenu.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'React'
+ 
+export const ExpandedMenu: FC = () => {
+    return(
+        <></>
+
+    );
+
+};
--- my-app/src/components/TopBar/TopBar.tsx
+++ my-app/src/components/TopBar/TopBar.tsx
@@ -2,22 +2,37 @@ import { FC } from 'React';
 import styled from 'styled-components';
 import { Wrapper } from'../../styledHelpers/Components'
 import { Colors } from '../../styledHelpers/Colors'
-
+import {ExpandedMenu} from '../TopBar/ExpandedMenu'
 
 
 const InnerWrapper = styled.div`
 width:1200px;
 background: ${Colors.white};
 `;
+const RightIcons = styled.div`
+
+`;
+const InputWrapper = styled.div`
+`;
 export const TopBar: FC = () => {
     return (
         <Wrapper>
             <InnerWrapper>
-                TopBar
-            </InnerWrapper>
-                
-
-        
+                <img src="" alt=""/>
+                <div>
+                    <ExpandedMenu />
+                </div>
+                <InputWrapper>
+                <input type="text"/>
+                <img src="" alt=""/>
+                </InputWrapper>
+            
+                <RightIcons>
+                <img src="" alt=""/>
+                <img src="" alt=""/>
+                <img src="" alt=""/>
+                </RightIcons>
+            </InnerWrapper>        
         </Wrapper>
 
 
